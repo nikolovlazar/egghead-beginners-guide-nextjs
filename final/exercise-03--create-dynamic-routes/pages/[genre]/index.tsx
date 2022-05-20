69943ffa0e634814d5b4c2b53161a7fffa0fd44b
alexmuriuki <alexmuriukimaina254@gmail.com>
2022-05-21 00:04:50 +0300

    include array mapping for songs, back link to home

--- final/exercise-03--create-dynamic-routes/pages/[genre]/index.tsx
+++ final/exercise-03--create-dynamic-routes/pages/[genre]/index.tsx
@@ -1,10 +1,18 @@
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Genre = () => {
   const router = useRouter()
   const { genre } = router.query
 
-  return <h2>I like {genre} music!</h2>
+  return (
+    <>
+      <h2>I like {genre} music!</h2>
+      <div>
+        <Link href="/">Back</Link>
+      </div>
+    </>
+  )
 }
 
 export default Genre
